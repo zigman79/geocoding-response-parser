34e634a166b8611cc583976938af09b7aec56dfc
Michael Labuschke <m.labuschke@futec-gmbh.de>
2020-10-16 11:08:35 +0200

    now its working

--- src/GeocodingResponseParser/ResponseParser.php
+++ src/GeocodingResponseParser/ResponseParser.php
@@ -5,15 +5,48 @@ class ResponseParser
 {
     protected $response;
     
-    public function __construct($response) {
+    public function __construct($response)
+    {
         $this->response = json_decode($response);
     }
 
-    public function dump() {
-        var_dump($this->response);
+    public function response()
+    {
+        return $this->response;
     }
 
-    public function getCity() {
+    private function findAddressType($search) {
+        if (!isset($this->response->results[0]->address_components)) {
+            return false;
+        }
+        foreach ($this->response->results[0]->address_components as $component) {
+            foreach ($component->types as $type) {
+                if ($type == $search) {
+                    return $component->long_name;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public function getCity()
+    {
+        return $this->findAddressType("locality");
+    }
 
+    public function getStreet() {
+        return $this->findAddressType("route");
+    }
+
+    public function getStreetNumber() {
+        return $this->findAddressType("street_number");
+    }
+
+    public function getFullStreet() {
+        return $this->getStreet() . " " .$this->getStreetNumber();
+    }
+
+    public function getCitycode() {
+        return $this->findAddressType("postal_code");
     }
 }
